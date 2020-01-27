bfdaeb5c1deb84d62b5b80a4cb2d008796ee1611
Jose R. Lopez <accounts@lopezinteractive.com>
2020-01-26 21:30:07 -0400

    Change Google Page Speed Insights url to slug

--- scripts/tfc-init.sh
+++ scripts/tfc-init.sh
@@ -35,7 +35,7 @@ sudo -u daemon -- mv -f /opt/bitnami/wordpress/wp-content/plugins/fresh-connect-
 info "Finished Installing custom plugins and grabing FCK's"
 
 info "Start Installing additional plugins ..."
-sudo -u daemon -- wp plugin install https://downloads.wordpress.org/plugin/google-pagespeed-insights.zip --force --activate --quiet
+sudo -u daemon -- wp plugin install google-pagespeed-insights --force --activate --quiet
 info "Finished Installing additional plugins"
 
 info "Activating custom plugins and grabing FCK's ..."
