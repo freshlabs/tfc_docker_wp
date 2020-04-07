786493403d65e6c3613dbc86c66224f92ee8e65f
Jose R. Lopez <accounts@lopezinteractive.com>
2020-04-07 14:47:36 -0400

    Added log info inside conditional

--- scripts/tfc-init.sh
+++ scripts/tfc-init.sh
@@ -7,7 +7,7 @@
 
 print_welcome_page
 
-info "Welcome to The Fresh Cloud, we just need to setup some additional things... rev 0.002"
+info "Welcome to The Fresh Cloud, we just need to setup some additional things... rev 0.003"
 
 # this seems redundant but necessary currently since the file at the root appears to be ignored by the nami_initialize wordpress function below
 info "Setup wp-cli config file ..."
@@ -16,9 +16,11 @@ sed -i '$ a\skip-plugins: true' /opt/bitnami/wp-cli/conf/wp-cli.yml
 info "Setup wp-cli config completed"
 
 if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "httpd" ]]; then
+    info "Doing some bitnami stuff ..."
     . /apache-init.sh
     . /wordpress-init.sh
     nami_initialize apache php mysql-client wordpress || true
+    info "Doing some bitnami stuff completed"
 fi
 # this is from the original app-entrypoint.sh
 
