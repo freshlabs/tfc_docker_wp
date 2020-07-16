c52d8f500150e3fad414acd7c9b3fc86787721ec
Jose R. Lopez <accounts@lopezinteractive.com>
2020-07-16 19:59:56 -0400

    Set executable bit

--- 5/debian-10/prebuildfs/build/bitnami-user.sh
+++ 5/debian-10/prebuildfs/build/bitnami-user.sh
@@ -4,3 +4,5 @@ useradd -ms /bin/bash bitnami
 mkdir -p /opt/bitnami
 sed -i -e 's/\s*Defaults\s*secure_path\s*=/# Defaults secure_path=/' /etc/sudoers
 echo 'bitnami ALL=NOPASSWD: ALL' >> /etc/sudoers
+
+# set executable bit
