3d454b9ec1b9360a7cb1763b3c0d1f26aed7a778
Carey Baird <careybaird@gmail.com>
2021-04-05 18:16:44 +0100

    Permissions on temp folder

--- 5/debian-10/prebuildfs/build/setup-tfc_wp.sh
+++ 5/debian-10/prebuildfs/build/setup-tfc_wp.sh
@@ -9,12 +9,13 @@
 # Setup Structure
 mkdir -p /bitnami/tfc_wp
 mkdir -p /opt/bitnami/tfc_wp
-mkdir -p /bitnami/tfc_wp/tmp/
+mkdir -p /bitnami/tfc_wp/tmp
 mkdir -p /bitnami/tfc_wp/.wp-cli/cache
 
 # Setup Permissions
 chmod 775 /opt/bitnami/tfc_wp
 chmod 775 /bitnami/tfc_wp
+chmod 775 /bitnami/tfc_wp/tmp
 chown -R 1001 /opt/bitnami/tfc_wp
 chown -R 1001 /bitnami/tfc_wp
 chown -R 1001:1001 /bitnami/tfc_wp/.wp-cli
