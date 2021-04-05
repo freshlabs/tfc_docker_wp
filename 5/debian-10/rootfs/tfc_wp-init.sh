325ccf3817f5ad8dec05227881a871e82b62c01e
Carey Baird <careybaird@gmail.com>
2021-04-05 15:22:07 +0100

    Removing volatile directory

--- 5/debian-10/rootfs/tfc_wp-init.sh
+++ 5/debian-10/rootfs/tfc_wp-init.sh
@@ -4,11 +4,12 @@
 . /opt/bitnami/base/functions
 . /opt/bitnami/base/helpers
 
-# Define some variables here
-# Volatile Storage Path
-VOLPATH="/opt/bitnami/tfc_wp";
-# Permanent Storage Path
-PERSPATH="/bitnami/tfc_wp";
+# Web Root
+ROOTPATH="/bitnami/tfc_wp";
+
+# Old volatile directory (deprecated)
+OLDROOTPATH="/opt/bitnami/tfc_wp";
+
 # Install File Location
 INSTALLFILE="$VOLPATH/.alreadyinstalled";
 # Latest Version File Location
@@ -26,15 +27,13 @@ REMOVEDEFAULTPLUGINS="yes"
 # Stop defining variables
 
 
-# Persistent/non-persistent is being removed. For now we just sync the two
-# TODO: Clean up and have a single directory
-info "Merging persistent and disposable directories."
-
-info "Copying all files to live directory"
-cp -rf "$PERSPATH" "$VOLPATH"
+# Persistent/non-persistent has been removed. This will sync the two
+info "Merging old persistent and disposable directories."
+info "Copying all files to new root directory"
+cp -rf "$OLDROOTPATH" "$ROOTPATH"
 
 info "Deleting files from old directory"
-rm -rf "$PERSPATH"/*
+rm -rf "$OLDROOTPATH"/*
 
 info "Finished merging persistent and disposable directories."
 
@@ -49,7 +48,7 @@ fi
 
 # Setup wp-cli cache to our permanent storage and export the variable
 info "Set wp-cli cache dir at persistent storage"
-export WP_CLI_CACHE_DIR="$VOLPATH"/.wp-cli/cache
+export WP_CLI_CACHE_DIR="$ROOTPATH"/.wp-cli/cache
 
 # Download WP Core files to disposable storage
 if [ ! -f "$LATESTVERSION" ]; then
@@ -69,7 +68,7 @@ else
 fi
 
   info "Downloading WP (version: $VERSIONONFILE) to Disposable Storage"
-  wp core download --version="$VERSIONONFILE" --locale=en_US --path="$VOLPATH" --force
+  wp core download --version="$VERSIONONFILE" --locale=en_US --path="$ROOTPATH" --force
 
 # We need to identify if we are dealing with a new install (or cleanup) or if this is an existing build
 if [ ! -f "$INSTALLFILE" ]; then
@@ -78,31 +77,31 @@ if [ ! -f "$INSTALLFILE" ]; then
 
   # Create the WP Config File
   info "Creating wp-config on Disposable Storage (placeholder)"
-  wp config create --dbhost="${MARIADB_HOST}:${MARIADB_PORT_NUMBER}" --dbname="${WORDPRESS_DATABASE_NAME}" --dbprefix="${WORDPRESS_TABLE_PREFIX}" --dbcharset=utf8 --dbuser="${WORDPRESS_DATABASE_USER}" --dbpass="${WORDPRESS_DATABASE_PASSWORD}" --locale=en_US --skip-check --path="$VOLPATH" --force
+  wp config create --dbhost="${MARIADB_HOST}:${MARIADB_PORT_NUMBER}" --dbname="${WORDPRESS_DATABASE_NAME}" --dbprefix="${WORDPRESS_TABLE_PREFIX}" --dbcharset=utf8 --dbuser="${WORDPRESS_DATABASE_USER}" --dbpass="${WORDPRESS_DATABASE_PASSWORD}" --locale=en_US --skip-check --path="$ROOTPATH" --force
 
   # Set some flags on wp-config file
   info "Making aditional adjustments to wp-config.php file"
-  wp config set WP_DEBUG false --raw --path="$VOLPATH"
-  wp config set WP_DEBUG_LOG false --raw --path="$VOLPATH"
-  wp config set WP_PLUGIN_DIR "$WPCONTENTDIR"/plugins --path="$VOLPATH"
-  wp config set FS_METHOD direct --path="$VOLPATH"
-  wp config set WP_TEMP_DIR "$WPTEMPDIR" --path="$VOLPATH"
+  wp config set WP_DEBUG false --raw --path="$ROOTPATH"
+  wp config set WP_DEBUG_LOG false --raw --path="$ROOTPATH"
+  wp config set WP_PLUGIN_DIR "$WPCONTENTDIR"/plugins --path="$ROOTPATH"
+  wp config set FS_METHOD direct --path="$ROOTPATH"
+  wp config set WP_TEMP_DIR "$WPTEMPDIR" --path="$ROOTPATH"
 
   # Make some changes to wp-config file
   info "Using SED to adjust our wp-config.php file with some additional settings"
-  sed -i "s/dirname( __FILE__ )/'"${VOLPATH//'/'/'\/'}"'/g" "$VOLPATH"/wp-config.php
+  sed -i "s/dirname( __FILE__ )/'"${ROOTPATH//'/'/'\/'}"'/g" "$ROOTPATH"/wp-config.php
 
   # Append some additional data to wp-config file
   info "Append some additional functions to wp-config.php file"
-  sed -i "\$aif ( \!defined( 'WP_CLI' ) ) \{\n\/\/  Disable pingback.ping xmlrpc method to prevent WordPress from participating in DDoS attacks\n\/\/  More info at: https://wiki.bitnami.com/Applications/Bitnami_WordPress#XMLRPC_and_Pingback\n\n// remove x-pingback HTTP header\nadd_filter('wp_headers', function(\$headers) \{\n            unset(\$headers['X-Pingback']);\n            return \$headers;\n           \});\n// disable pingbacks\nadd_filter( 'xmlrpc_methods', function( \$methods ) \{\n             unset( \$methods['pingback.ping'] );\n             return \$methods;\n           \});\n}" "$VOLPATH"/wp-config.php
+  sed -i "\$aif ( \!defined( 'WP_CLI' ) ) \{\n\/\/  Disable pingback.ping xmlrpc method to prevent WordPress from participating in DDoS attacks\n\/\/  More info at: https://wiki.bitnami.com/Applications/Bitnami_WordPress#XMLRPC_and_Pingback\n\n// remove x-pingback HTTP header\nadd_filter('wp_headers', function(\$headers) \{\n            unset(\$headers['X-Pingback']);\n            return \$headers;\n           \});\n// disable pingbacks\nadd_filter( 'xmlrpc_methods', function( \$methods ) \{\n             unset( \$methods['pingback.ping'] );\n             return \$methods;\n           \});\n}" "$ROOTPATH"/wp-config.php
 
   # Run WP Install Procedure
   info "Let us run the install now"
   #wp core install --url=localhost --title="aaaa" --admin_user="momon" --admin_password="1234" --admin_email="redstormj@gmail.com" --path="/opt/bitnami/tfc_wp" --skip-email
-  wp core install --url="${WORDPRESS_BLOG_URL}" --title="${WORDPRESS_BLOG_NAME}" --admin_user="${WORDPRESS_USERNAME}" --admin_password="${WORDPRESS_PASSWORD}" --admin_email="${WORDPRESS_EMAIL}" --path="$VOLPATH" --skip-email
+  wp core install --url="${WORDPRESS_BLOG_URL}" --title="${WORDPRESS_BLOG_NAME}" --admin_user="${WORDPRESS_USERNAME}" --admin_password="${WORDPRESS_PASSWORD}" --admin_email="${WORDPRESS_EMAIL}" --path="$ROOTPATH" --skip-email
 
   info "Creating placeholder files"
-  touch "$VOLPATH"/.htaccess
+  touch "$ROOTPATH"/.htaccess
 
 
   info "Setup placeholder install file"
@@ -111,8 +110,8 @@ if [ ! -f "$INSTALLFILE" ]; then
   if [ $REMOVEDEFAULTPLUGINS = "yes" ]; then
     
     info "Removing default wordpress plugins"
-    rm -rf "$VOLPATH"/wp-content/plugins/akismet/
-    rm -f "$VOLPATH"/wp-content/plugins/hello.php
+    rm -rf "$ROOTPATH"/wp-content/plugins/akismet/
+    rm -f "$ROOTPATH"/wp-content/plugins/hello.php
     info "Default Wordpress Plugins Removed"
 
   fi
@@ -124,42 +123,42 @@ if [ ! -f "$INSTALLFILE" ]; then
 fi
 
 info "Fixing permissions"
-chown -h 1001 "$VOLPATH"/wp-config.php
-chown -h 1001 "$VOLPATH"/wp-content
+chown -h 1001 "$ROOTPATH"/wp-config.php
+chown -h 1001 "$ROOTPATH"/wp-content
 
 info "Cleanup files for tidyness"
-rm -rf "$VOLPATH"/wp-config-sample.php
-rm -rf "$VOLPATH"/license.txt
-rm -rf "$VOLPATH"/readme.html
+rm -rf "$ROOTPATH"/wp-config-sample.php
+rm -rf "$ROOTPATH"/license.txt
+rm -rf "$ROOTPATH"/readme.html
 
 info "Make more changes to wp-config file (add custom function)"
-yes | cp -rf /freshlabs.php "$VOLPATH"/freshlabs.php
+yes | cp -rf /freshlabs.php "$ROOTPATH"/freshlabs.php
 
 info "Delete old wpverinject function"
-sed -i --follow-symlinks '/wpverinject();/d' "$VOLPATH"/wp-config.php
-sed -i --follow-symlinks -n '/$table_prefix =/{p; :a; N; /WP_DEBUG/!ba; s/.*\n//}; p' "$VOLPATH"/wp-config.php
+sed -i --follow-symlinks '/wpverinject();/d' "$ROOTPATH"/wp-config.php
+sed -i --follow-symlinks -n '/$table_prefix =/{p; :a; N; /WP_DEBUG/!ba; s/.*\n//}; p' "$ROOTPATH"/wp-config.php
 
 info "Delete previous require/freshlabs command"
-sed -i --follow-symlinks "/freshlabs.php/d" "$VOLPATH"/wp-config.php
+sed -i --follow-symlinks "/freshlabs.php/d" "$ROOTPATH"/wp-config.php
 
 info "Add new require/freshlabs command"
-sed -i --follow-symlinks "\$arequire('"$VOLPATH"/freshlabs.php');" "$VOLPATH"/wp-config.php
+sed -i --follow-symlinks "\$arequire('"$ROOTPATH"/freshlabs.php');" "$ROOTPATH"/wp-config.php
 
 # Consider importing SQL file if conditions met
-if [ $MIGRATE_DB_TO_LOCAL = "yes" ] && [ -f "$VOLPATH/$MIGRATE_WORDPRESS_DATABASE_NAME-migrate.sql" ] && [ ! -f "$VOLPATH/.migratedsql" ]; then
+if [ $MIGRATE_DB_TO_LOCAL = "yes" ] && [ -f "$ROOTPATH/$MIGRATE_WORDPRESS_DATABASE_NAME-migrate.sql" ] && [ ! -f "$ROOTPATH/.migratedsql" ]; then
 
   # we will need to force recreate the wpconfig file
   info "Sinve we've migrated to a local environment we now need to fix our wp-config file"
-  wp config set DB_NAME "${WORDPRESS_DATABASE_NAME}" --path="$VOLPATH"
-  wp config set DB_USER "${WORDPRESS_DATABASE_USER}" --path="$VOLPATH"
-  wp config set DB_PASSWORD "${WORDPRESS_DATABASE_PASSWORD}" --path="$VOLPATH"
-  wp config set DB_HOST "${MARIADB_HOST}:${MARIADB_PORT_NUMBER}" --path="$VOLPATH"
+  wp config set DB_NAME "${WORDPRESS_DATABASE_NAME}" --path="$ROOTPATH"
+  wp config set DB_USER "${WORDPRESS_DATABASE_USER}" --path="$ROOTPATH"
+  wp config set DB_PASSWORD "${WORDPRESS_DATABASE_PASSWORD}" --path="$ROOTPATH"
+  wp config set DB_HOST "${MARIADB_HOST}:${MARIADB_PORT_NUMBER}" --path="$ROOTPATH"
 
   info "Found DB Import file, importing it now..."
-  wp db import $VOLPATH/$MIGRATE_WORDPRESS_DATABASE_NAME-migrate.sql --path="$VOLPATH"
+  wp db import $ROOTPATH/$MIGRATE_WORDPRESS_DATABASE_NAME-migrate.sql --path="$ROOTPATH"
 
   info "Setup placeholder db imported file (.migratedsql)"
-  touch "$VOLPATH/.migratedsql"
+  touch "$ROOTPATH/.migratedsql"
 
 fi
 
@@ -169,11 +168,11 @@ if [ $ADDCUSTOMPLUGINS = "yes" ]; then
 
   # Install Fresh Connect Plugin
   info "Installing Fresh Connect"
-  wp plugin install fresh-connect --force --activate --path="$VOLPATH"
+  wp plugin install fresh-connect --force --activate --path="$ROOTPATH"
 
   # Install Google Pagespeed
   info "Installing Google Pagespeed"
-  wp plugin install google-pagespeed-insights --force --activate --path="$VOLPATH"
+  wp plugin install google-pagespeed-insights --force --activate --path="$ROOTPATH"
 
   info "Install custom or additional plugins completed"
   # this is prime time to setup additional plugins (only on first install)
@@ -181,13 +180,13 @@ if [ $ADDCUSTOMPLUGINS = "yes" ]; then
   # Grab FCK's 
   info "Now grabbing FCK"
   echo "Fresh Connect Key >>"
-  wp option get fp_connection_keys --path="$VOLPATH"
+  wp option get fp_connection_keys --path="$ROOTPATH"
   echo "<< Fresh Connect Key"
   # Grab FCK's 
 fi
 
 info "Make sure both Wordpress and DB are up to date"
-wp core update --version="$VERSIONONFILE" --path="$VOLPATH"
-wp core update-db --path="$VOLPATH"
+wp core update --version="$VERSIONONFILE" --path="$ROOTPATH"
+wp core update-db --path="$ROOTPATH"
 
 info "All done!"
